8cba59d9b093d5016aad7ca73ea1e7dad180fc62
Vgisaac <isaaczurigomez4.vela5@gmail.com>
2026-05-15 12:46:17 -0600

    cambio necesario.

--- tests/Feature/Settings/ProfileUpdateTest.php
+++ tests/Feature/Settings/ProfileUpdateTest.php
@@ -20,10 +20,6 @@ class ProfileUpdateTest extends TestCase
 
     public function test_profile_information_can_be_updated(): void
     {
-        $user = User::factory()->create();
-
-        $this->actingAs($user);
-
         $response = Livewire::test('pages::settings.profile')
             ->set('name', 'Test User')
             ->set('email', 'test@example.com')
