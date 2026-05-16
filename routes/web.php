23ccd175bcb9c12058eec2a78e6618de6a8b381b
Vgisaac <isaaczurigomez4.vela5@gmail.com>
2026-05-16 14:18:30 -0600

    cambio para una prueba en home

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/admin');
+Route::redirect('/', '/admin')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
